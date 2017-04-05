309cb8e3fca4cc599d5a3cd881d0d342309ce6cb
muria590 <muria590@newschool.edu>
2017-04-05 19:47:08 -0400

    javascript quiz

---
+++ projects/javascriptquiz/assets/main.js
@@ -0,0 +1,88 @@
+console.log('hello world')
+
+var SpringBreak = [
+'skiing while there was a storm',
+'eating pizza',
+'hanging out with my family'
+];
+
+for ( var i = 0; i < SpringBreak.length; i++ ) {
+  console.log( SpringBreak[i] + ' was fun ' );
+}
+
+var groupOne = ['Ana', 'Gaurika', 'Moon'];
+var groupTwo = ['Victoria', 'Liz', 'Jissaura'];
+var groupThree = ['Kyle', 'Carol', 'Fish'];
+
+var allStudentsHere=[
+  'Carol Chong',
+  'Gaurika Milan Dalal',
+  'Vitoria Feitosa Di Iulio',
+  'Nafisa Afrin Iqbal',
+  'Moonnyung Jo',
+  'Ana Sofia Murillo',
+  'Jissaura Taveras Hernandez',
+  'Elizabeth Tsai',
+  'Kyle Yu',
+];
+
+var allStudentsHere = false;
+
+var studentsMissing =['Gyuri Nam'];
+
+var studentsMissing = true;
+
+var arrayOfArrays =[
+groupOne,
+groupTwo,
+groupThree];
+
+for ( var index1 = 0; i < arrayOfArrays.length; i++ ) {
+  console.log( 'this group is number ' + (i + 1 ));
+  console.log( 'The students in this group are');
+
+
+  for (var index2 = 0; index2.length <arrayOfArrays[index1]; index2++){
+      console.log(arrayOfArrays[index1][index2]);
+  }
+  }
+
+var stringName = ['ANA SOFIA'] //string name
+  console.log( stringName );
+
+var stringNumber = ['308'] //string number
+  console.log( stringNumber);
+
+var booleanValue= [true] //boolean value
+  console.log( booleanValue  );
+
+
+var arrayLetters= ['a', 'n', 'a'] //array letters of my name
+  console.log( arrayLetters );
+
+var arrayData=['ana', 2 , true] //array 3 different data types
+ console.log(arrayData);
+
+var twoStrings=['ANA  ' + 'MURILLO'] //add two strings name
+console.log(twoStrings);
+
+var oneH =(30 + 70) //oneHundred
+console.log(oneH);
+
+
+for ( var i = 0; i < arrayLetters.length; i++ ) {
+  console.log( arrayLetters[0] + arrayLetters[2] );
+}
+
+var word =['supercalifragilisticexpialidocious']
+
+for ( var i = 0; i < word.length(); i++ ) {
+  console.log( word.length() );
+}
+
+
+//missing 3 and 4
+
+var allStudentsHere = true;
+
+
